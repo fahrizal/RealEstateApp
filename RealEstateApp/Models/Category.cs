480130bce1924684e7f730f20974371e8e704156
Fahrizal Bin Fahruddin <fahrizal.fahruddin@gmail.com>
2024-05-08 18:22:57 +0800

    Add project files.

---
+++ RealEstateApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace RealEstateApp.Models;
+
+public class Category
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("image_url")]
+    public string ImageUrl { get; set; }
+
+    [JsonProperty("properties")]
+    public object Properties { get; set; }
+}
---
+++ RealEstateApp/Models/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstateApp.Models;
+
+public class Register
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+}
